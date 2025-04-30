d49d29b558fd3526948104dee4f957714cd7789e
Shivam <shivam.s72@kalvium.community>
2025-04-30 10:53:30 +0530

    done

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Form from '../pages/Form';
-import Home from '../pages/home';
+import Home from '../pages/Home';
 import PropertySearchPage from '../pages/PropertySearchPage';
 import EncumbrancePage from '../pages/EncumbrancePage';
 import AboutUs from '../pages/AboutUs';
